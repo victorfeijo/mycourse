9142f7257ed77a2ce20cc43590658d4cdfc912ef
Victor Feijo <victor.feijoa@gmail.com>
2016-05-31 22:50:02 -0300

    testing a routing system

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "html"
+    "log"
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    bind := fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
+    log.Fatal(http.ListenAndServe(bind, router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
