626b6b4b768900f68ce20381f701c6168d0db6d8
Victor Feijo <victor.feijoa@gmail.com>
2016-06-10 20:12:45 -0300

    added the logger to router

--- router.go
+++ router.go
@@ -9,11 +9,16 @@ func NewRouter() *mux.Router {
 
         router := mux.NewRouter().StrictSlash(true)
         for _, route := range routes {
+                var handler http.Handler
+
+                handler = route.HandlerFunc
+                handler = Logger(handler, route.Name)
+
                 router.
-                Methods(route.Method).
-                Path(route.Pattern).
-                Name(route.Name).
-                Handler(route.HandlerFunc)
+                        Methods(route.Method).
+                        Path(route.Pattern).
+                        Name(route.Name).
+                        Handler(route.HandlerFunc)
         }
 
         return router;
